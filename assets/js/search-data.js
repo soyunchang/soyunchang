b16fc32df08b7fa5896533ddd614d6dd6cd13ea2
soyunchang <soyun.chang1@gmail.com>
2025-08-18 20:29:31 +0000

    Deploying to gh-pages from @ soyunchang/soyunchang@14eb089f6b47c4a0305dd5f53641631fb11082ba 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -32,7 +32,7 @@ ninja.data = [{
           },
         },{id: "nav-cv",
           title: "cv",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
+          description: "Please find my CV available here.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/soyunchang/cv/";
