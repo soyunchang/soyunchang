cd39ba315aff2f6aa83fa7f054bddb8f0ef345ba
soyunchang <soyun.chang1@gmail.com>
2025-08-18 21:26:21 +0000

    Deploying to gh-pages from @ soyunchang/soyunchang@f798d96bedde2745aa4488211a80ea27734e1bfa 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -30,13 +30,6 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/soyunchang/publications/";
           },
-        },{id: "nav-repositories",
-          title: "repositories",
-          description: "Edit the `_data/repositories.yml` and change the `github_users` and `github_repos` lists to include your own GitHub profile and repositories.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/soyunchang/repositories/";
-          },
         },{id: "nav-teaching",
           title: "teaching",
           description: "Materials for courses you taught. Replace this text with your description.",
