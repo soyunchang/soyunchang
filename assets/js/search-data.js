6196ec4c768f7f564bddbb5989c402b17c954079
soyunchang <soyun.chang1@gmail.com>
2025-08-19 15:47:54 +0000

    Deploying to gh-pages from @ soyunchang/soyunchang@12aaac74a58b3ecf203fe7f1be42e292fd29ec5b 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -416,9 +416,9 @@ ninja.data = [{
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
           description: "",
-          section: "News",},{id: "projects-project-1",
-          title: 'project 1',
-          description: "with background image",
+          section: "News",},{id: "projects-in-international-organizations-we-trust",
+          title: 'In International Organizations We Trust?',
+          description: "Explaining Public Support of WHO Policy Recommendations",
           section: "Projects",handler: () => {
               window.location.href = "/soyunchang/projects/1_project/";
             },},{id: "projects-project-2",
