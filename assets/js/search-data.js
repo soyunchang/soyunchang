aba45bda3427ad540cab0789697797f00b0b8b51
soyunchang <soyun.chang1@gmail.com>
2025-08-19 17:13:42 +0000

    Deploying to gh-pages from @ soyunchang/soyunchang@e991a7d6f6d48b5bf8c00ecf4d1f195ce2c1dbe8 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -441,11 +441,6 @@ ninja.data = [{
           description: "",
           section: "Projects",handler: () => {
               window.location.href = "/soyunchang/projects/5_project/";
-            },},{id: "projects-project-6",
-          title: 'project 6',
-          description: "a project with no image",
-          section: "Projects",handler: () => {
-              window.location.href = "/soyunchang/projects/6_project/";
             },},{id: "projects-project-9",
           title: 'project 9',
           description: "another project with an image 🎉",
